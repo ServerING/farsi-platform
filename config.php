b323c3c92f8e4e4320ae1a022062e2b6f17e30b0
مسعود حدادان <masoudhadadan@gmail.com>
2025-03-04 11:07:14 +0330

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// تنظیمات پایه
+define('BASE_URL', 'https://yourdomain.com');
+define('LIB_DIR', __DIR__ . '/libraries/');
+
+// امنیتی
+ini_set('display_errors', 0);
+error_reporting(0);
+?>
